db4865a1a4e7639ed049135d41f9eab2c00b7798
Jill Lin <JillLin@Melaleuca.com>
2021-08-06 17:05:34 +0800

    Rename from 001 to Case ID.

--- Test Suites/Testrail_Web_Suite1.ts
+++ Test Suites/Testrail_Web_Suite1.ts
@@ -15,36 +15,42 @@
       <guid>67175025-76b7-44ca-bc09-3401fd1a27ff</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/_Web/003.Chrome 30 and higher</testCaseId>
+      <testCaseId>Test Cases/_Web/C245298 - Log in page is secure (https in the url)</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2de2d19a-efb3-4fdb-ba5e-657ac66e6eee</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/_Web/004.Check Cant Access Your Account</testCaseId>
+      <testCaseId>Test Cases/_Web/C245304 - Check Cant Access Your Account</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2b978d78-a38d-4a40-9916-9017c62f2bd5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/_Web/005.Check Create Your Online Account</testCaseId>
+      <testCaseId>Test Cases/_Web/C245306 - Check Create Your Online Account</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>e7b7a755-41fa-45fe-8f51-75bae4e5dc39</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/_Web/008.Verify General Error page comes up correctly</testCaseId>
+      <testCaseId>Test Cases/_Web/C245309 - Verify General Error page comes up correctly</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>cee4af92-896b-4751-9a0a-588a192363fb</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/_Web/011.Go to Contact Melaleuca</testCaseId>
+      <testCaseId>Test Cases/_Web/C245312 - Go to Contact Melaleuca</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>23d71a47-7627-4b16-95e5-ed19926f3525</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/_Web/020.Add configurable SKU via Quick Shop</testCaseId>
+      <testCaseId>Test Cases/_Web/039. My Account -- Cancellation</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8ca2c66b-ffab-4617-8e48-92653e95e16a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/_Web/049. Business Center -- MORE</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
