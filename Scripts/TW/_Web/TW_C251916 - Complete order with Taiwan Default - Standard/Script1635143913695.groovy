774eb963419f86d7401f3793bf7cc0d2e512ae5c
Jill Lin <jilllin@melaleuca.com>
2021-10-26 10:13:13 +0800

    c251916-revised

---
+++ Scripts/TW/_Web/TW_C251916 - Complete order with Taiwan Default - Standard/Script1635143913695.groovy
@@ -0,0 +1,76 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(GlobalVariable.url_uatwweb)
+
+WebUI.refresh()
+
+WebUI.maximizeWindow()
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input_Email_UserName'), GlobalVariable.tw_input_username1)
+
+WebUI.setEncryptedText(findTestObject('Object Repository/OB003/Page_/input__Password'), GlobalVariable.tw_input_password1)
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button_ (2)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button_(0)'))
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input_()_txtSkuEntry'), '1210.2')
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1 (13)'))
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input_()_txtSkuEntry'), '2000.2')
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1 (13)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1_2 (11)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1_2_3 (7)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_Melaleuca The Wellness Company/a__1_2 (22)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1_2_3_4 (7)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1_2_3_4_5 (3)'))
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input__AddressToEdit.AddressLine1 (3)'), 'testaddress3')
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input__AddressToEdit.AddressLine2 (3)'), 'testaddress4')
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1_2_3_4_5_6 (1)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1_2_3_4_5_6 (1)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1_2_3_4_5_6_7 (1)'))
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input__paymethodCvv (1)'), '123')
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1_2_3_4_5_6_7_8 (1)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_Melaleuca The Wellness Company/a__1_2_3'))
+
+WebUI.closeBrowser()
+
+'Complete order with Taiwan Default - Standard'
+GlobalVariable.test_case = '251916'
+
+WebUI.callTestCase(findTestCase('TW/TW Testrail reporting'), [:], FailureHandling.STOP_ON_FAILURE)
+
