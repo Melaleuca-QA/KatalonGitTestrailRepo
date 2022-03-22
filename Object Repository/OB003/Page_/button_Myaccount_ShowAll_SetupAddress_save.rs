fe9b7a5d17e38ac78dca418c78fe353a209052d6
Jill Lin <jilllin@melaleuca.com>
2022-03-22 11:15:40 +0800

    branch issue1 testing

--- Object Repository/OB003/Page_/button_Myaccount_ShowAll_SetupAddress_save.rs
+++ Object Repository/OB003/Page_/button_Myaccount_ShowAll_SetupAddress_save.rs
@@ -1,16 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>button__1_2_3_4 (5)</name>
+   <name>button_Myaccount_ShowAll_SetupAddress_save</name>
    <tag></tag>
-   <elementGuidId>b2e2a836-0c73-4400-b6f9-f66b53b0ccd2</elementGuidId>
+   <elementGuidId>77aac03c-5dd4-4899-b7fc-e07878b6d3f3</elementGuidId>
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>(//button[@id='btnDelete'])[2]</value>
+         <value>//button[@id='saveAddressBtn']</value>
       </entry>
       <entry>
          <key>CSS</key>
+         <value>#saveAddressBtn</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -27,83 +28,83 @@
       <matchCondition>equals</matchCondition>
       <name>class</name>
       <type>Main</type>
-      <value>pure-button pure-button-blue</value>
+      <value>saveAndCancel pure-button pure-button-green pure-button-35</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>id</name>
       <type>Main</type>
-      <value>btnDelete</value>
-   </webElementProperties>
-   <webElementProperties>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>onclick</name>
-      <type>Main</type>
-      <value>DeleteAddress('85B95695509AC05A1EE5A6E72778EA0987861D9A');</value>
+      <value>saveAddressBtn</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>type</name>
       <type>Main</type>
-      <value>button</value>
+      <value>submit</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>text</name>
       <type>Main</type>
-      <value>刪除</value>
+      <value>儲存</value>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;adrList_ReplacementContainer&quot;)/table[@class=&quot;addressListHeader&quot;]/tbody[1]/tr[3]/td[@class=&quot;addressButtonColumn&quot;]/div[1]/button[@id=&quot;btnDelete&quot;]</value>
+      <value>id(&quot;saveAddressBtn&quot;)</value>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:attributes</name>
       <type>Main</type>
-      <value>(//button[@id='btnDelete'])[2]</value>
+      <value>//button[@id='saveAddressBtn']</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:idRelative</name>
       <type>Main</type>
-      <value>//div[@id='adrList_ReplacementContainer']/table/tbody/tr[3]/td[3]/div/button</value>
+      <value>//div[@id='simplePageContent']/form/div/div[5]/div/button</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='台灣'])[2]/following::button[1]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='電話號碼'])[1]/following::button[1]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='hsipaddress4'])[1]/following::button[1]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='取消'])[1]/preceding::button[1]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='編輯'])[2]/preceding::button[1]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='消費回饋'])[3]/preceding::button[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='儲存']/parent::*</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:position</name>
       <type>Main</type>
-      <value>//tr[3]/td[3]/div/button</value>
+      <value>//div[5]/div/button</value>
    </webElementXpaths>
 </WebElementEntity>
---
+++ Scripts/HK/_Web/HK_C245366 - Add a new address - Copy/Script1646987559650.groovy
@@ -0,0 +1,81 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.callTestCase(findTestCase('_Login_hk'), [:], FailureHandling.STOP_ON_FAILURE)
+
+'close birthday\'s shopping'
+if (WebUI.waitForElementVisible(findTestObject('OB003/Page_Melaleuca The Wellness Company/div__stars'), 3)) {
+    WebUI.click(findTestObject('Object Repository/OB003/Page_Melaleuca The Wellness Company/i__fa fa-times-circle'))
+}
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_Melaleuca The Wellness Company/span_ (2)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_Melaleuca The Wellness Company/a_ (3)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/span_ (3)'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/button__1 (2) (1)'))
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input__AddressToEdit.ShipToName_1 (1)'), 'hsipaddress')
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/OB003/Page_/select__1_2 (1) (1)'), '852', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/OB003/Page_/select__1_2_3 (1) (1)'), '香港', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/OB003/Page_/select__1_2_3_4 (1)'), '中環', true)
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input__AddressToEdit.AddressLine1_1 (1)'), 'hsipaddress1')
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input_,,(03,11,2)_AddressToEdit.AddressLine2 (1)'), 'hsipaddress2')
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input__AddressToEdit.DeliveryInstruction_1 (1)'), 'do not ship')
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input__AddressToEdit.FormattedPhoneNumber (1)'), '85251654321')
+
+WebUI.click(findTestObject('OB003/Page_/button_Myaccount_ShowAll_SetupAddress_save'))
+
+WebUI.click(findTestObject('OB003/Page_/button_Myaccount_ShowAll_SetupAddress_Edit'))
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input__AddressToEdit.AddressLine1_1 (1)'), 'hsipaddress3')
+
+WebUI.setText(findTestObject('Object Repository/OB003/Page_/input_,,(03,11,2)_AddressToEdit.AddressLine2 (1)'), 'hsipaddress4')
+
+WebUI.click(findTestObject('OB003/Page_/button_Myaccount_ShowAll_SetupAddress_save'))
+
+WebUI.click(findTestObject('OB003/Page_/button_Myaccount_ShowAll_SetupAddress_Delete'))
+
+WebUI.click(findTestObject('Object Repository/OB003/Page_/a__1 (3)'))
+
+WebUI.callTestCase(findTestCase('_Logout'), [:], FailureHandling.STOP_ON_FAILURE)
+
+'Add a new address'
+GlobalVariable.test_case = '245366'
+
+WebUI.callTestCase(findTestCase('HK/HK Testrail reporting'), [:], FailureHandling.STOP_ON_FAILURE)
+
+'Edit an existing'
+GlobalVariable.test_case = '245367'
+
+WebUI.callTestCase(findTestCase('HK/HK Testrail reporting'), [:], FailureHandling.STOP_ON_FAILURE)
+
+'Delete an exisiting address'
+GlobalVariable.test_case = '245368'
+
+WebUI.callTestCase(findTestCase('HK/HK Testrail reporting'), [:], FailureHandling.STOP_ON_FAILURE)
+
